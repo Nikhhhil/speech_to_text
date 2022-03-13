2789646c1075825d364a4441cc48733738a6cc4a
nikhil kushwah <84187202+Nikhhhil@users.noreply.github.com>
2022-03-13 21:11:44 +0530

    update

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,40 +1,35 @@
-# importing libraries 
+
 import speech_recognition as sr 
 import os 
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
-
-# create a speech recognition object
 r = sr.Recognizer()
-
-# a function that splits the audio file into chunks
-# and applies speech recognition
 def get_large_audio_transcription(path):
     """
     Splitting the large audio file into chunks
     and apply speech recognition on each of these chunks
     """
-    # open the audio file using pydub
+    # open the audio file 
     sound = AudioSegment.from_wav(path)  
-    # split audio sound where silence is 700 miliseconds or more and get chunks
+    # split audio sound where silence is 700 miliseconds
     chunks = split_on_silence(sound,
         # experiment with this value for your target audio file
-        min_silence_len = 500,
+        min_silence_len = 600,
         # adjust this per requirement
         silence_thresh = sound.dBFS-14,
         # keep the silence for 1 second, adjustable as well
-        keep_silence=500,
+        keep_silence=600,
     )
-    folder_name = "audio-chunks"
+    f_name = "audio-chunks"
     # create a directory to store the audio chunks
-    if not os.path.isdir(folder_name):
-        os.mkdir(folder_name)
+    if not os.path.isdir(f_name):
+        os.mkdir(f_name)
     whole_text = ""
     # process each chunk 
     for i, audio_chunk in enumerate(chunks, start=1):
         # export audio chunk and save it in
         # the `folder_name` directory.
-        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+        chunk_filename = os.path.join(f_name, f"chunk{i}.wav")
         audio_chunk.export(chunk_filename, format="wav")
         # recognize the chunk
         with sr.AudioFile(chunk_filename) as source:
